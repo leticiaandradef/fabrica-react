9bdf1e3bd592314fb2551b94d47714e938892aa4
ℓeticia ๋࣭⭑ <159487910+leticiaandradef@users.noreply.github.com>
2025-02-24 13:50:46 -0400

    Update page.js

--- sweet-candy/src/app/vendaCupcake/page.js
+++ sweet-candy/src/app/vendaCupcake/page.js
@@ -1,4 +1,4 @@
-import styles from './agradecimento.module.css'
+import styles from './page.module.css'
 import Link from 'next/link'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
