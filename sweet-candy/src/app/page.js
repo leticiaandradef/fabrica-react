747120cea7ac490dade4a4b791fe18f59166d5d2
leticiaandradef <lelectpm2021@gmail.com>
2025-06-17 17:31:37 -0400

    comit

--- sweet-candy/src/app/page.js
+++ sweet-candy/src/app/page.js
@@ -1,5 +1,4 @@
 'use client'
-
 import React, { useState } from 'react';
 import styles from './page.module.css';
 import Link from 'next/link';
@@ -14,58 +13,84 @@ const Checkout = () => {
     alert('Carrinho limpo!');
   };
 
-
   return (
     <div>
       <Header />
 
-    <div className={styles.container}>
-      <h1 className={styles.h1}>Checkout</h1>
-      <h2 className={styles.h2}>Formas de Pagamento</h2>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>Checkout</h1>
+        <h2 className={styles.h2}>Formas de Pagamento</h2>
 
-      <div className={styles.pagamentos}>
-        <label className={styles.label}>
-          <input className={styles.input} type="radio" name="pagamento" onChange={() => setPagamento('pix')} /> Pix
-        </label>
-        <label className={styles.label}>
-          <input className={styles.input} type="radio" name="pagamento" onChange={() => setPagamento('maquina')} /> Máquina móvel
-        </label>
-        <label className={styles.label}>
-          <input className={styles.input} type="radio" name="pagamento" onChange={() => setPagamento('dinheiro')} /> Dinheiro
-        </label>
+        <div className={styles.pagamentos}>
+          <label className={styles.label}>
+            <input className={styles.input} type="radio" name="pagamento" onChange={() => setPagamento('pix')} /> Pix
+          </label>
+          <label className={styles.label}>
+            <input className={styles.input} type="radio" name="pagamento" onChange={() => setPagamento('maquina')} /> Máquina móvel
+          </label>
+          <label className={styles.label}>
+            <input className={styles.input} type="radio" name="pagamento" onChange={() => setPagamento('dinheiro')} /> Dinheiro
+          </label>
+        </div>
+
+        <h3 className={styles.h3}>Endereço de entrega</h3>
+        <div className={styles.bloco}>
+          <p className={styles.p}>
+            Rua:
+            <input type="text" className={styles.input} placeholder="Digite a rua" />
+          </p>
+          <p className={styles.p}>
+            Número da casa:
+            <input
+              type="number"
+              className={styles.input}
+              placeholder="Digite o número"
+              inputMode="numeric"
+              pattern="[0-9]*"
+            />
+          </p>
+          <p className={styles.p}>
+            CEP:
+            <input
+              type="number"
+              className={styles.input}
+              placeholder="Digite o CEP"
+              inputMode="numeric"
+              pattern="[0-9]*"
+            />
+          </p>
+          <p className={styles.p}>
+            Bairro:
+            <input type="text" className={styles.input} placeholder="Digite o bairro" />
+          </p>
+          <p className={styles.p}>
+            Complemento:
+            <input type="text" className={styles.input} placeholder="Ex: apto 101" />
+          </p>
+        </div>
+
+        <h3 className={styles.resumotitle}>Resumo do pedido</h3>
+        <div className={styles.blocoresumo}>
+          <p className={styles.p}>Quantidade:</p>
+          <p className={styles.p}>Taxa de serviço:</p>
+          <p className={styles.p}>Taxa de entrega:</p>
+          <p className={styles.p}>Total:</p>
+        </div>
+
+        <div className={styles.botoes}>
+          <button className={styles.button} onClick={handleLimparCarrinho}>
+            <Link className={styles.link} href="#">Limpar Carrinho</Link>
+          </button>
+          <button className={styles.button}>
+            <Link className={styles.link} href="/pedido">Voltar</Link>
+          </button>
+          <button className={styles.button}>
+            <Link className={styles.link} href="/vendaCupcake">Fazer pedido</Link>
+          </button>
+        </div>
       </div>
 
-      <h3 className={styles.h3}>Endereço de entrega</h3>
-      <div className={styles.bloco}>
-        <p className={styles.p}>Rua:</p>
-        <p className={styles.p}>Número da casa:</p>
-        <p className={styles.p}>CEP:</p>
-        <p className={styles.p}>Bairro:</p>
-        <p className={styles.p}>Complemento:</p>
-      </div>
-
-      <h3 className={styles.resumotitle}>Resumo do pedido</h3>
-      <div className={styles.blocoresumo}>
-        <p className={styles.p}>Quantidade:</p>
-        <p className={styles.p}>Taxa de serviço:</p>
-        <p className={styles.p}>Taxa de entrega:</p>
-        <p className={styles.p}>Total:</p>
-      </div>
-
-      <div className={styles.botoes}>
-        <button className={styles.button} onClick={handleLimparCarrinho}>
-          <Link className={styles.link} href="/">Limpar Carrinho</Link>
-        </button>
-        <button className={styles.button}> 
-          <Link className={styles.link} href="/pedido">Voltar</Link>
-        </button>
-        <button className={styles.button}>
-          <Link className={styles.link} href="/vendaCupcake">Fazer pedido</Link>
-        </button>
-      </div>
-     </div>
-
-    <Footer />
+      <Footer />
     </div>
   );
 };
